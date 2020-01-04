cd2a95a56f4ad414144ade6351297ec419beaff6
Pablo Radomski <radomskip@gmail.com>
2020-01-04 15:55:39 -0300

    Se convierte <Logged> a un componente funcional

--- src/components/layout/Logged.js
+++ src/components/layout/Logged.js
@@ -3,26 +3,24 @@ import React, { Component } from 'react';
 import { Header, Sidebar } from 'components/navigation';
 import { ListItemLink } from 'components/list';
 
-class Logged extends Component {
-  render() {
-    return (
-      <div className="container">
-        <Sidebar>
-          <ul>
-            <ListItemLink to="/products" icon="list">Products listing</ListItemLink>
-            <ListItemLink to="/products/new" icon="add">Prodct add</ListItemLink>
-          </ul>
-        </Sidebar>
-        
-        <div className="main">
-          <Header />
-          <div className="content">
-            {React.cloneElement({ ...this.props }.children, { ...this.props })}
-          </div>
+const Logged = (props) => {
+  return (
+    <div className="container">
+      <Sidebar>
+        <ul>
+          <ListItemLink to="/products" icon="list">Products listing</ListItemLink>
+          <ListItemLink to="/products/new" icon="add">Prodct add</ListItemLink>
+        </ul>
+      </Sidebar>
+      
+      <div className="main">
+        <Header />
+        <div className="content">
+          {React.cloneElement(props.children, props)}
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default Logged;
